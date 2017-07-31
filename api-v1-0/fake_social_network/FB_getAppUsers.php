044638e98e655c65f2b64a12da47275a0daaf086
spineag <spineag@gmail.com>
2017-07-31 11:38:55 +0300

    smth

--- api-v1-0/fake_social_network/FB_getAppUsers.php
+++ api-v1-0/fake_social_network/FB_getAppUsers.php
@@ -14,6 +14,27 @@ $arU = ['1566615670077474'];
 //    $arU[] = $u['social_id'];
 //}
 
+// add from user real neighbors
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $shardDb = $app->getShardDb($_POST['userId'], $channelId);
+    $result = $shardDb->query('SELECT * FROM user_friends WHERE user_id='.$_POST['userId']);
+    $ar = $result->fetch();
+    $idso=[];
+    if ($ar['friend_1'] != '0') $idso[]=(int)$ar['friend_1'];
+    if ($ar['friend_2'] != '0') $idso[]=(int)$ar['friend_2'];
+    if ($ar['friend_3'] != '0') $idso[]=(int)$ar['friend_3'];
+    if ($ar['friend_4'] != '0') $idso[]=(int)$ar['friend_4'];
+    if ($ar['friend_5'] != '0') $idso[]=(int)$ar['friend_5'];
+    if (count($idso) > 0) {
+        $qwe = implode(',', array_map('intval', $idso));
+        $result = $mainDb->query("SELECT social_id FROM users WHERE id IN (".$qwe.")");
+        $ar = $result->fetchAll();
+        foreach ($ar as $key => $u) {
+            $arU[] = $u['social_id'];
+        }
+    }
+}
+
 $ids = implode(',', array_map('intval', $arU));
 $result = $mainDb->query("SELECT id, social_id, first_name, last_name, photo_url FROM users WHERE social_id IN (".$ids.")");
 $ar = $result->fetchAll();
