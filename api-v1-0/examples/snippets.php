af438d1e74479648a7c4752ef479e1d0374d4001
spineag <spineag@gmail.com>
2017-08-09 12:15:53 +0300

    smth

--- api-v1-0/examples/snippets.php
+++ api-v1-0/examples/snippets.php
@@ -1,2 +1,8 @@
 <?php
 //ALTER TABLE selo_1.user_quest ADD CONSTRAINT abQuest UNIQUE(user_id, quest_id);
+
+// дублікати
+//SELECT t1.* FROM farm_1.user_quest_task AS t1
+//    LEFT JOIN (SELECT id FROM farm_1.user_quest_task GROUP BY user_id, task_id) AS t2
+//        ON t1.id = t2.id
+//    WHERE t2.id IS NULL
