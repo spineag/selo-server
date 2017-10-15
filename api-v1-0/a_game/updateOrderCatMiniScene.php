9e8c6f67d567a10f940f32596d66d9da82846cab
spineag <spineag@gmail.com>
2017-10-16 01:07:32 +0300

    small fix

--- api-v1-0/a_game/updateOrderCatMiniScene.php
+++ api-v1-0/a_game/updateOrderCatMiniScene.php
@@ -8,33 +8,33 @@ if (isset($_POST['userId']) && !empty($_POST['userId'])) {
     $channelId = (int)$_POST['channelId'];
 
     if ($app->checkSessionKey($_POST['userId'], $_POST['sessionKey'], $channelId)) {
-        $m = md5($_POST['userId'].$_POST['notificationNew'].$app->md5Secret());
+        $m = md5($_POST['userId'].$_POST['orderCatScene'].$app->md5Secret());
         if ($m != $_POST['hash']) {
             $json_data['id'] = 6;
-            $json_data['status'] = 's400';
+            $json_data['status'] = 's434';
             $json_data['message'] = 'wrong hash';
             echo json_encode($json_data);
         } else {
             try {
                 $shardDb = $app->getShardDb($_POST['userId'], $channelId);
-                $result = $shardDb->query('UPDATE user_info SET notification_new ="' . $_POST['notificationNew'] . '" WHERE user_id=' . $_POST['userId']);
+                $result = $shardDb->query('UPDATE user_info SET o_cat_scene="'.$_POST['orderCatScene'].'" WHERE user_id='.$_POST['userId']);
                 if (!$result) {
                     $json_data['id'] = 2;
-                    $json_data['status'] = 's337';
+                    $json_data['status'] = 's435';
                     throw new Exception("Bad request to DB!");
                 }
 
-                $json_data['message'] = '';
+                $json_data['message'] = 'ok';
                 echo json_encode($json_data);
             } catch (Exception $e) {
-                $json_data['status'] = 's184';
+                $json_data['status'] = 's433';
                 $json_data['message'] = $e->getMessage();
                 echo json_encode($json_data);
             }
         }
     } else {
         $json_data['id'] = 13;
-        $json_data['status'] = 's221';
+        $json_data['status'] = 's436';
         $json_data['message'] = 'bad sessionKey';
         echo json_encode($json_data);
     }
@@ -42,7 +42,7 @@ if (isset($_POST['userId']) && !empty($_POST['userId'])) {
 else
 {
     $json_data['id'] = 1;
-    $json_data['status'] = 's185';
+    $json_data['status'] = 's179';
     $json_data['message'] = 'bad POST[userId]';
     echo json_encode($json_data);
 }
