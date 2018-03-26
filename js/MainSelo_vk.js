363187679c05ffe8fcb012abdc406113d0ac3db4
spineag <spineag@gmail.com>
2018-03-26 16:21:21 +0300

    adds for vk posting

--- js/MainSelo_vk.js
+++ js/MainSelo_vk.js
@@ -46,7 +46,7 @@ var SN = function (social) { // social == 2
         }, '5.73');
     };
 
-    that.vk_api = function(e){
+    that.vk_api = function(e) {
         console.log('MainSelo_vk:: try vk_api method: ' + e.method);
         e.params.test_mode = 1;
         VK.api(e.method, e.params, function (d){
@@ -63,6 +63,59 @@ var SN = function (social) { // social == 2
     that.callMethod = function(m,p) {
         VK.callMethod.apply(null, [m].concat(p));
     };
+    VK.addCallback('onOrderSuccess', function(order_id) {
+        that.flash().orderSuccessHandler(order_id);
+    });
+    VK.addCallback('onOrderFail', function() {
+        that.flash().orderFailHandler();
+    });
+    VK.addCallback('onOrderCancel', function() {
+        that.flash().orderCancelHandler();
+    });
+
+    that.wallpost = function(e) { //e: uid, message, url, fSuccess, fCancel
+        try {
+            console.log('ABC wallpost url image: ' + e.url);
+            VK.api('photos.getWallUploadServer', {
+                uid: e.uid
+            }, function (data) {
+                console.log('ABC - getWallUploadServer UPLOAD URL: ');
+                console.log(data);
+                if (data.response) {
+                    console.log('ABC try vk upload');
+                    $.post('js/upload_photo_vk.php', {
+                        upload_url: data.response.upload_url,
+                        image_url: e.url
+                        },
+                        function (json) {
+                            console.log('ABC - uploadPhotoVK:');
+                            console.log(json);
+                            VK.api("photos.saveWallPhoto", {
+                                server: json.server,
+                                photo: json.photo,
+                                hash: json.hash,
+                                uid: e.uid
+                                }, function (data) {
+                                    console.log('ABC - saveWallPhoto:');
+                                    console.log(data);
+                                    VK.api('wall.post', {
+                                            message: e.message,
+                                            attachments: data.response['0'].id
+                                        },
+                                        function (d) {
+                                            console.log('4 - vk wallpost answer');
+                                            console.log(d);
+                                        });
+                            });
+                        },
+                        'json');
+                }
+            });
+        } catch(err) {
+            console.log(' try wallpost error:');
+            console.log(err);
+        }
+    }
 
 };
 
