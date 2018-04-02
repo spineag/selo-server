15351cd4ae83a0ee563f08f6198f6f7850db68d7
SashaNesterchuk <nesterchuksasha95@gmail.com>
2018-04-02 16:25:23 +0300

    Party

---
+++ api-v1-0/a_game/deleteUserParty.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/selo-project/php/api-v1-0/library/Application.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/selo-project/php/api-v1-0/library/defaultResponseJSON.php');
+
+if (isset($_POST['userId']) && !empty($_POST['userId'])) {
+    $app = Application::getInstance();
+    $userId = filter_var($_POST['userId']);
+    $channelId = (int)$_POST['channelId'];
+    $shardDb = $app->getShardDb($userId, $channelId);
+
+    if ($app->checkSessionKey($_POST['userId'], $_POST['sessionKey'], $channelId)) {
+        try {
+            $result = $shardDb->query('DELETE FROM user_party WHERE id_party='.$_POST['idParty'].' AND user_id = ' . $userId);
+            if ($result) {
+                $json_data['message'] = '';
+                echo json_encode($json_data);
+            } else {
+                $json_data['id'] = 2;
+                $json_data['status'] = 's249';
+                throw new Exception("Bad request to DB!");
+            }
+        }
+        catch (Exception $e)
+        {
+            $json_data['status'] = 's058';
+            $json_data['message'] = $e->getMessage();
+            echo json_encode($json_data);
+        }
+    } else {
+        $json_data['id'] = 13;
+        $json_data['status'] = 's221';
+        $json_data['message'] = 'bad sessionKey';
+        echo json_encode($json_data);
+    }
+}
+else
+{
+    $json_data['id'] = 1;
+    $json_data['status'] = 's059';
+    $json_data['message'] = 'bad POST[userId]';
+    echo json_encode($json_data);
+}
